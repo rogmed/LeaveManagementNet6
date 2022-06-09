29037721294cb56a0481ae4684a1e0dbf0e9ef25
Rogelio Medina <rogeliomdn@gmail.com>
2022-06-10 01:25:24 +0200

    Setup GenericRepository

---
+++ LeaveManagement.Web/Repositories/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using LeaveManagement.Web.Contracts;
+using LeaveManagement.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Web.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity != null)
+            {
+                _context.Set<T>().Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            var entity = await GetAsync(id);
+            return entity != null;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<int> GetCountAsync()
+        {
+            var list = await GetAllAsync();
+            return list.Count;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            //_context.Entry(entity).State = EntityState.Modified; // This is the same as next line
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
