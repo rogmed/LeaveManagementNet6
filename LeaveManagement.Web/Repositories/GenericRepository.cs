395a6b928ab848c0d05981eb537e4334613fa5bf
Rogelio Medina <rogeliomdn@gmail.com>
2022-06-23 03:42:42 +0200

    Added IGenericRepository<T> and ILeaveTypeRepository, refactored LeaveTypeController accordingly

--- LeaveManagement.Web/Repositories/GenericRepository.cs
+++ LeaveManagement.Web/Repositories/GenericRepository.cs
@@ -40,8 +40,12 @@ namespace LeaveManagement.Web.Repositories
             return await _context.Set<T>().ToListAsync();
         }
 
-        public async Task<T> GetAsync(int id)
+        public async Task<T?> GetAsync(int? id)
         {
+            if (id == null)
+            {
+                return null;
+            }
             return await _context.Set<T>().FindAsync(id);
         }
 
